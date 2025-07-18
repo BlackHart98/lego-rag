47a91eda6588945f5fa081cadb9488232b235760
Pius Arhanbhunde <pjacks419@gmail.com>
2025-07-18 20:47:32 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,47 +4,6 @@ from dataclasses import dataclass
 from enum import IntEnum
 
 
-@dataclass
-class SplitResult:
-    file_path: str
-    file_check_sum: str
-    split_check_sum: str
-    md_header_split: t.Any
-    embedding: t.Any | None = None
- 
- 
-@dataclass
-class FileMeta:
-    file_path: str
-    split_count: int
-    split_results: t.List[SplitResult]
-    
-    
-@dataclass
-class FileMetaV2:
-    file_path: str
-    split_count: int
-    split_results: t.List[SplitResult]
-    
-    
-    
-@dataclass    
-class MistralModel:
-    model_id: str = "mistral"
-    embedding_model: str = "mistral-embed"
-    llm: str = ""
-
-
-
-class AIModel:
-    def __init__(self, model_id: str):
-        match model_id:
-            case MistralModel.model_id:
-                 self.model = MistralModel
-            case _:
-                raise ValueError("Unsupported model id")
-
-
 class FileType:
     NIL = ""
     MARKDOWN = ".md"
